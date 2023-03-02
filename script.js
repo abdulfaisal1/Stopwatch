00b90dfc50d6d3d4e2ea4f89927a3237d84dc4b5
abdulfaisal1 <abdulfaisal7447@gmail.com>
2023-03-02 16:35:30 +0530

    'first'

---
+++ script.js
@@ -0,0 +1,50 @@
+//access the elements
+//access the display timer using DOM
+let Display=document.querySelector('.DisplayTimer')
+//initialoize the values to zero
+let[hours,minutes,seconds,milliseconds]=[0,0,0,0]
+let int=null
+
+//start
+document.getElementById('startBtn').addEventListener('click',()=>{
+    if(int!==null){
+        clearInterval(int)
+    }
+    int=setInterval(DisplayTimer,10)
+}) 
+
+//stop
+document.getElementById('stopBtn').addEventListener('click',()=>{
+    clearInterval(int)
+})
+
+//reset
+document.getElementById('resetBtn').addEventListener('click',()=>{
+    clearInterval(int)   
+    hours=minutes=milliseconds=seconds=0;
+    Display.innerHTML=`00:00:00:00`
+})
+
+//display timer function
+function DisplayTimer(){
+    milliseconds++
+    if(milliseconds==1000){
+        milliseconds=0
+        seconds++
+        if (seconds==60){
+            seconds=0
+            minutes++
+            if(minutes==60){
+                minutes=0
+                hours++
+            }
+        }
+    }
+    let h=hours<10? '0' +hours:hours
+    let m=milliseconds<10? '0' +minutes:minutes
+    let s=seconds<10? '0' +seconds:seconds
+    let ms=milliseconds<10? '00' +milliseconds:milliseconds <100?'0'+milliseconds:milliseconds
+    Display.innerHTML=`${h}:${m}:${s}:${ms}`
+
+}
+
